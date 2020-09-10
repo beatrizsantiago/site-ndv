1bc5ee2d1d0c7e5e2ac180bf2a1539db84a26056
Beatriz Santiago <beatrizsantiago1999@hotmail.com>
2020-09-10 13:56:04 -0300

    Página de projetos

--- _js/index.js
+++ _js/index.js
@@ -30,6 +30,43 @@ $(document).ready(function () {
         slidesToScroll: 1,
         autoplay: true,
         autoplaySpeed: 5000,
+        arrows: false,
+    })
+})
+
+$(document).ready(function () {
+    $('.images').slick({
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        dots: true,
+        autoplay: true,
+        autoplaySpeed: 5000,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
     })
 })
 
